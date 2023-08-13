44865e5c2a15cce742e4fc6fb7572e10bac73c23
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-13 20:18:13 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,22 @@ import React from "react";
 import {useState} from 'react'
 import './../styles/App.css';
 import Modal from './Modal'
+
 const App = () => {
   let [state,Setstate]= useState(false)
   function onClose(){
     Setstate(false)
   }
+  window.addEventListener('click',(event)=>{
+    let btn=document.getElementById('startbtn')
+    if(event.target!=btn && (event.target.className!='model-overlay'&& event.target.className!='model-close'&& event.target.className!='model p')){
+      console.log(event)
+      Setstate(false)
+    }
+  })
   return (
     <div>
-        <button onClick={()=>{Setstate(true)}}>Show Modal</button>
+        <button onClick={()=>{Setstate(true)}} id='startbtn'>Show Modal</button>
         {state && <Modal oncall={onClose} />}
     </div>
   )
