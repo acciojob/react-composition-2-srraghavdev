eed0c17d9d11479a3f457e04c6261ca5cdd0145b
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-15 11:23:14 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,25 @@
 
-// import React from "react";
-// import {useState} from 'react'
-// import './../styles/App.css';
-// import Modal from './Modal'
-
-// const App = () => {
-//   let [state,Setstate]= useState(false)
-//   function onClose(){
-//     Setstate(false)
-//   }
-//   window.addEventListener('click',(event)=>{
-//     let btn=document.getElementById('startbtn')
-//     if(event.target!=btn && (event.target.className!='model-overlay'&& event.target.className!='model-close'&& event.target.className!='model p')){
-//       console.log(event)
-//       Setstate(false)
-//     }
-//   })
-//   return (
-//     <div>
-//         <button onClick={()=>{Setstate(true)}} id='startbtn'>Show Modal</button>
-//          <Modal oncall={onClose} state={state} />
-//     </div>
-//   )
-// }
-
-// export default App
 import React from "react";
+import {useState} from 'react'
 import './../styles/App.css';
-import ShowModal from "./ShowModal";
+import ShowModal from './ShowModal'
 
 const App = () => {
+  let [state,Setstate]= useState(false)
+  function onClose(){
+    Setstate(false)
+  }
+  window.addEventListener('click',(event)=>{
+    let btn=document.getElementById('startbtn')
+    if(event.target.className =='model-overlay' || event.target.className=='model-close' || event.target.className=='model p'){
+      console.log(event)
+      Setstate(false)
+    }
+  })
   return (
     <div>
-        {/* Do not remove the main div */}
-        <ShowModal />
+        <button onClick={()=>{Setstate(true)}} id='startbtn'>Show Modal</button>
+         <ShowModal oncall={onClose} state={state} />
     </div>
   )
 }
